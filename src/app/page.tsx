add8b37eb236c7dbeefbddca4c0902a0b2698d07
Juan José Rodriguez <zoranbow@gmail.com>
2024-11-25 14:09:28 -0500

    homePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import Link from 'next/link';
+
 export default function homePage() {
-  return <main></main>;
+  return (
+    <main className="min-h-screen flex justify-center items-center">
+      <div className="flex flex-col w-full max-w-xs rounded-lg border-2 border-foreground p-3 gap-4">
+        <h1 className='text-center text-xl font-bold'>Whispers</h1>
+        <Link href="/login" className='rounded-sm border-2 border-foreground text-center font-bold py-2 hover:bg-foreground hover:text-background'>
+          Login
+        </Link>
+        <Link href="/signup" className='rounded-sm border-2 border-foreground text-center font-bold py-2 hover:bg-foreground hover:text-background'>
+          Sign Up
+        </Link>
+      </div>
+    </main>
+  );
 }
