7e16661ad9ab0e388df6126716e10a17fcfcfd72
Juan José Rodriguez <zoranbow@gmail.com>
2024-12-02 09:32:05 -0500

    basic navbar

--- src/app/(private)/navbar.tsx
+++ src/app/(private)/navbar.tsx
@@ -1,5 +1,13 @@
+import Link from 'next/link';
+
 export default function Navbar() {
   return (
-    <nav className="bg-green-600 bg-opacity-20 text-center">NAVBAR</nav>
+    <nav className="bg-green-600 bg-opacity-20 flex items-center justify-evenly text-xs">
+      <Link href="/feed">FEED</Link>
+      <Link href="/profile">PROFILE</Link>
+      <Link href="/followers">FOLLOWERS</Link>
+      <Link href="/following">FOLLOWING</Link>
+      <Link href="/admin">ADMIN</Link>
+    </nav>
   );
 }
